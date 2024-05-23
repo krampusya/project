d00aa95234c078063e68cb66b297832845908f9e
Oleg <example@gmail.com>
2024-05-23 15:28:16 +0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,80 @@
+/* eslint-disable no-unused-vars */
+"use strict"
+ 
+// /* eslint-disable no-unused-vars */
+// const arr = ['a', 'b', 'c'];
+
+// arr[10] = '3456';
+
+// console.log(arr);
+
+// const arrObj = {
+//     a: 'a',
+//     '1': 'b',
+//     2: 'c',
+//     abc: {
+//         df: [{}, {}],
+//         def: {
+
+//         }
+//     }
+// };
+
+// const b = 'b';
+
+// // arrObj.b = '1234';
+// arrObj['b'] = [];
+
+
+// console.log(arrObj['b']);
+// console.log(arrObj.b);
+
+// console.log(arr[1]);
+// console.log(arrObj[1]);
+
+// // const obj = {a: 1, b: 2};
+// const obj = {
+//     'Anna': 500,
+//     'Alice': 800
+// };
+
+
+
+
+// const storeName = 'Krampus';
+
+// const storeDescription = {
+//     budget: 10000,
+//     employees: ['Oleg', 'Oktavian', 'Kolya'],
+//     products: {
+//         'iphone': 1000,
+//         'macbook': 2000
+//     },
+//     open: true
+
+// };
+
+// alert('Hello');
+
+// const result = confirm('Are you here?');
+// console.log(result);
+
+// const answer = +prompt('Вам є 18?', '18');
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt('Яке ваше імʼя?', '');
+// answers[1] = prompt('Яке ваше прізвище?', '');
+// answers[2] = prompt('Скільки вам років?', '');
+
+// console.log(typeof(answers));
+// console.log(typeof(null));
+
+const category = 'toys';
+
+console.log(`https://someurl.com/${category}/5`);
+
+const user = 'Oleg';
+
+alert(`Привіт, ${user}`);
---
+++ js/script2.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-unused-vars */
+"use sctrict";
+
+let number = 4.6;
+
+console.log(4/0);
+console.log('string' * 9);
+
+const persone = 'Alex';
+
+const bool = false;
+
+// console.log(something);
+
+let und;
+console.log(und);
+
+const obj = {
+    name: 'Oleg',
+    age: 23,
+    isMarried: false
+};
+// console.log(obj.name);
+console.log(obj['name']);
+
+let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+console.log(arr[1]);
