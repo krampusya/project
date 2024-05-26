6e4cbf51bf401ea111fdab89dac9700e4044c4f6
Oleg <example@gmail.com>
2024-05-27 00:29:44 +0300

    Логічні оператори

--- js/script.js
+++ js/script.js
@@ -121,22 +121,75 @@
 
 // (num === 50) ? console.log('ok!') : console.log('error');
 
-const num = 50;
+// const num = 50;
 
-switch (num) {
-    case 49: 
-        console.log('Ну і лапа');
-        break;
-    case 100:
-        console.log('Ну і лапа');
-        break;
-    case 51:
-        console.log('Nice');
-        break;
-    default: 
-        console.log('Не влазиш');
-        break;
+// switch (num) {
+//     case 49: 
+//         console.log('Ну і лапа');
+//         break;
+//     case 100:
+//         console.log('Ну і лапа');
+//         break;
+//     case 51:
+//         console.log('Nice');
+//         break;
+//     default: 
+//         console.log('Не влазиш');
+//         break;
+// }
+
+// Логічні оператори
+
+// const hamburger = 3;
+// const fries = 1;
+// const cola = 0;
+
+// console.log(hamburger === 3 && cola 1 && fries);
+
+// console.log(1 && 0);
+// console.log(1 && 5);
+// console.log(null && 5);
+// console.log(0 && 'ok');
+ 
+// if (hamburger === 3 && cola === 1 && fries) {
+//     console.log('Ви їсте');
+// } else {
+//     console.log('Ми йдемо');
+
+// const hamburger = 3;
+// const fries = 1;
+// const cola = 0;
+
+// console.log(hamburger === 3 && cola 1 && fries);
+
+// console.log(1 && 0);
+// console.log(1 && 5);
+// console.log(null && 5);
+// console.log(0 && 'ok');
+ 
+// if (hamburger === 3 && cola === 1 && fries) {
+//     console.log('Ви їсте');
+// } else {
+//     console.log('Ми йдемо');
+
+const hamburger = 3;
+const fries = 3;
+const cola = 0;
+const nuggets = 2;
+
+ 
+if (hamburger === 3 && cola === 2 || fries === 3 && nuggets) {
+    console.log('Ви їсте');
+} else {
+    console.log('Ми йдемо');
 }
+
+console.log(hamburger === 3 && cola === 2 || fries === 3 && nuggets);
+
+
+let johnReport, alexReport, samReaport, mariaReport = "done";
+
+console.log(johnReport || alexReport || samReaport || mariaReport);
 
 
 
