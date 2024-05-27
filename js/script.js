c09f4c8adb8fa02a78c439f5298d65f2ab02d1e2
Oleg <example@gmail.com>
2024-05-27 15:45:18 +0300

    Цикли додадковий

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-redeclare */
+/* eslint-disable no-empty */
 /* eslint-disable no-unused-vars */
  
 "use strict"
@@ -214,4 +216,38 @@
 // for (let i = 1; i < 0; i++) {
 //     console.log(num);
 //     num++;
+// }
+
+// Цикли додадковий 
+
+// for (let i = 0; i < 3; i++) {
+//     console.log(i);
+//     for (let j = 0; j < 3; j++){
+//         console.log(j);
+//     }
+// }
+
+// let result = '';
+// const length = 7;
+
+// for (let i = 1; i < length; i++) {
+
+//     for (let j = 0; j < i; j++) {
+//     result += '*';
+//     }
+
+//     result += '\n';
+// }
+
+// console.log(result);
+
+// fitst: for (let i = 0; i < 3; i++) {
+//     console.log(`First level: ${i}`);
+//     for (let j = 0; j < 3; j++){
+//         console.log(`Second level: ${j}`);
+//         for (let k = 0; k < 3; k++){
+//             if (k === 2) break fitst;
+//             console.log(`Third level: ${k}`);
+//         }
+//     }
 // }
