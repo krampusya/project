c4b3e917ed60e8ac5253b0fe1055e755cd6436bc
Oleg <example@gmail.com>
2024-05-28 23:20:58 +0300

    practice 3

--- js/script.js
+++ js/script.js
@@ -522,3 +522,78 @@
 // const test = '12.2px';
 // console.log(parseInt(test));
 // console.log(parseFloat(test));
+
+// practice 3
+
+
+// let numberOfFilms;
+
+// function start(){
+//     numberOfFilms = +prompt('Скільки фільмів Ви вже подивились?', '');
+
+//     while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+//         numberOfFilms = +prompt('Скільки фільмів Ви вже подивились?', '');
+//     }
+// }
+
+// start();
+
+
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
+
+
+// function rememberMyFilms(){
+//     for (let i = 0; i < 2; i++) {
+//         const a = prompt('Один з останніх переглянутих фільмів?', ''),
+//         b = prompt('На скільки оціните його', '');
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log('done');
+//     } else {
+//         console.log('error');
+//         i--;
+//     }
+//     }
+// }
+
+// rememberMyFilms();
+
+// function detectedPersonalLevel(){
+//     if (personalMovieDB.count < 10) {
+//         console.log('Переглянуто доволі мало фільмів');
+//     } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30){
+//         console.log('Ви класичний глядач');
+//     } else if (personalMovieDB.count > 30){
+//         console.log('Ви кіноман');
+//     } else {
+//         console.log('Відбулась помилка')
+//     }
+// }
+
+// detectedPersonalLevel();
+
+// function showMyDB(hidden){
+//     if (!hidden) {
+//         console.log(personalMovieDB);
+//     }
+// }
+
+// showMyDB(personalMovieDB.privat);
+
+// function writeYourGenres(){
+//     for (let i = 1; i <= 3; i++) {
+        
+//         personalMovieDB.genres[i - 1] = prompt(`Ваш улюблений жанр під номером ${i}`);
+//     }
+// }
+
+// writeYourGenres();
+
+// console.log(personalMovieDB);
+
