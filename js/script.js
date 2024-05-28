a1fadbfcb552186a3479d43edec28d602787abff
Oleg <example@gmail.com>
2024-05-28 17:38:11 +0300

    Аргументи Функції

--- js/script.js
+++ js/script.js
@@ -397,39 +397,60 @@
 // console.log(personalMovieDB);
 
 // Функції
-let num = 20;
+// let num = 20;
 
-function showFirstMessage(text) {
-    console.log(text);
-    console.log(num);
-}
+// function showFirstMessage(text) {
+//     console.log(text);
+//     console.log(num);
+// }
 
-showFirstMessage('Hello Wolrd');
-console.log(num);
+// showFirstMessage('Hello Wolrd');
+// console.log(num);
 
-function calc(a, b) {
-    return a + b;
+// function calc(a, b) {
+//     return a + b;
 
-}
+// }
 
-console.log(calc(4, 3));
-console.log(calc(5, 6));
-console.log(calc(10, 6));
+// console.log(calc(4, 3));
+// console.log(calc(5, 6));
+// console.log(calc(10, 6));
 
-function ret() {
-    let num = 50;
+// function ret() {
+//     let num = 50;
 
-    //
+//     //
 
-    return num;
-}
+//     return num;
+// }
 
-const anotherNum = ret();
-console.log(anotherNum);
+// const anotherNum = ret();
+// console.log(anotherNum);
 
 
-const logger = function() {
-    console.log('Hello');
-};
+// const logger = function() {
+//     console.log('Hello');
+// };
 
-logger();
+// logger();
+
+// const calc = (a, b) => { 
+//     console.log('1');
+//     return a + b
+
+// };
+// console.log(calc(4, 3));
+// console.log(calc(5, 6));
+// console.log(calc(10, 6));
+
+// Аргументи Функції
+
+// const usdCurr = 28;
+// const eurCurr = 32;
+
+// function convert (amount, curr) {
+//     console.log(curr * amount);
+// }
+
+// convert(500, usdCurr);
+// convert(500, eurCurr);
