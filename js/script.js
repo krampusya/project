941bb5deaf164b760142c337d5d0d90cce91f9c1
Oleg <example@gmail.com>
2024-05-31 01:23:38 +0300

    Обєкти, деструктуризація обєктів

--- js/script.js
+++ js/script.js
@@ -670,31 +670,72 @@
 
 // callback !!!
 
-function fitst(){
-    // Do something
-    setTimeout(function(){
-        console.log(1);
-    }, 500);
-}
+// function fitst(){
+//     // Do something
+//     setTimeout(function(){
+//         console.log(1);
+//     }, 500);
+// }
 
-function second(){
-    // Do something
-    console.log(2);
+// function second(){
+//     // Do something
+//     console.log(2);
 
-}
-fitst();
-second();
+// }
+// fitst();
+// second();
 
-function learnJS(lang, callback){
-    console.log(`Я вчу: ${lang}`);
-    callback();
-}
+// function learnJS(lang, callback){
+//     console.log(`Я вчу: ${lang}`);
+//     callback();
+// }
 
-function done(){
-    console.log('Я закінчив');
-}
+// function done(){
+//     console.log('Я закінчив');
+// }
 
-learnJS('JavaScript', done);
+// learnJS('JavaScript', done);
+
+// Обєкти, деструктуризація обєктів
+
+// const obj = new Object();
+// const options = {
+//     name: 'test',
+//     width: 1024,
+//     height: 1024,
+//     colors: {
+//         border: 'black',
+//         bg: 'red'
+//     },
+//     makeTest: function(){
+//         console.log('Test');
+//     }
+// };
+
+// options.makeTest();
+
+// const {border, bg} = options.colors;
+// console.log(border);
+
+// console.log(Object.keys(options).length); // Outputs the number of keys in the options object
+
+// let counter = 0;
+
+// for (let key in options) {
+//     if (typeof options[key] === 'object') {
+//         for (let i in options[key]) {
+//             console.log(`Властивість: ${i}, значення: ${options[key][i]}`);
+//         }
+//     } else {
+//         console.log(`Властивість: ${key}, значення: ${options[key]}`);
+//         counter++;
+//     }
+// }
+
+// console.log(counter); // Outputs the count of non-object properties in the options object
+
+
+
 
 
 
