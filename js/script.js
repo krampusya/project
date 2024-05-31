38961e52626abaaf18c82a53069ad7bb0c2b3a63
Oleg <example@gmail.com>
2024-05-31 14:24:14 +0300

    Spread

--- js/script.js
+++ js/script.js
@@ -771,6 +771,105 @@
 // products.sort();
 // console.log(products.join('; '));
 
+// !! Перелача даних по посиланню та значенню, Spread оператор (ES6-ES9) !!
+
+// let a = 5,
+//     b = a;
+
+// b = b + 5;
+// console.log(b); // Outputs 10
+// console.log(a); // Outputs 5
+
+// const obj = {
+//     a: 5,
+//     b: 1
+// };
+
+// const copy = obj; //Посилання
+
+// copy.a = 10;
+
+// console.log(copy); // Outputs
+// console.log(obj); // Outputs
+
+// function copy (mainObj) {
+//     let objCopy = {};
+
+//     let key;
+//     for (key in mainObj) {
+//         objCopy[key] = mainObj[key];
+//     }
+
+//     return objCopy;
+// }
+
+// const numbers = {
+//     a: 2,
+//     b: 5,
+//     c: {
+//         x: 7,
+//         y: 4
+//     }
+// };
+
+// const newNumbers = copy(numbers);
+
+// newNumbers.a = 10;
+// newNumbers.c.x = 10;
+
+// // console.log(newNumbers);
+// // console.log(numbers);
+
+// const add = {
+//     d: 17,
+//     e: 20
+// };
+
+// const clone = Object.assign({}, add);
+
+// clone.d = 20;
+
+// // console.log(add);
+// // console.log(clone);
+
+// const oldArray = ['a', 'b', 'c'];
+// const newArray = oldArray.slice();
+
+// newArray[1] = 'fuck';
+
+// console.log(newArray);
+// console.log(oldArray);
+
+// const video = ['youtube', 'vimeo', 'netflix'],
+//       blogs = ['wordpress', 'livejournal', 'blogger'],
+//       internet = [...video, ...blogs, 'instagram', 'facebook'];
+
+// console.log(internet);
+
+// function log(a, b, c){
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+// }
+
+// const num = [2, 5, 7];
+
+// log(...num);
+
+// const array = ['a', 'b', 'c'];
+
+// const newAarray = [...array];
+
+// const q = {
+//     one: 1,
+//     two: 2
+// };
+
+// const newObj = {
+//     ...q
+// };
+
+
 
 
 
